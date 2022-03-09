04f3a20bf71b509ed277c2cce251a2e7f81f9ab7
LeeYongwoo-kor <newlyo1205@naver.com>
2022-03-09 23:57:13 +0900

    Add layout components to each page

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,18 +1,21 @@
 import { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="divide-y-[1px] py-10">
-      <div className="flex items-center space-x-3 px-4 py-3">
-        <div className="aspect-square w-12 rounded-full bg-slate-300" />
-        <div className="cursor-pointer">
-          <p className="text-gray-700">Mr Lee</p>
-          <p className="text-sm text-gray-600">
-            See you tomorrow in the corner at 2pm!
-          </p>
+    <Layout hasTabBar title="Chat">
+      <div className="divide-y-[1px] py-10">
+        <div className="flex items-center space-x-3 px-4 py-3">
+          <div className="aspect-square w-12 rounded-full bg-slate-300" />
+          <div className="cursor-pointer">
+            <p className="text-gray-700">Mr Lee</p>
+            <p className="text-sm text-gray-600">
+              See you tomorrow in the corner at 2pm!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
