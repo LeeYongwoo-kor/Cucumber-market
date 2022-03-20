d31a520e594344ae1c00e35bea01788d14851e94
LeeYongwoo-kor <newlyo1205@naver.com>
2022-03-21 02:59:57 +0900

    Add SMS Service from twilio of trial mode

--- components/button.tsx
+++ components/button.tsx
@@ -16,7 +16,7 @@ export default function Button({
     <button
       {...rest}
       className={cls(
-        "text white w-full rounded-md border border-transparent bg-green-600 px-4 font-medium shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2",
+        "w-full rounded-md border border-transparent bg-green-600 px-4 font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2",
         large ? "py-3 text-base" : "py-2 text-sm"
       )}
     >
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const twilioClienct = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   request: NextApiRequest,
@@ -26,6 +29,15 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    await twilioClienct.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Welcome to Cucumber Market! Sending you a message from the Cucumber Market. Enter 6 digits of the token received from message. Your login token is ${payload}.`,
+    });
+  }
+  // to: phone -> Theoeretically, This code is correct
+
   return response.json({
     ok: true,
   });
