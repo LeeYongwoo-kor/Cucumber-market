43b09cf89cacc45b8db18ba7ef71d8515b109f4d
LeeYongwoo-kor <newlyo1205@naver.com>
2022-03-20 23:44:50 +0900

    Add user, token logic of handler(Prisma)

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,34 @@
-import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(request: NextApiRequest, response: NextApiResponse) {
-  return response.status(200).end();
+async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = request.body;
+  const user = phone ? { phone } : email ? { email } : null;
+  if (!user) return response.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  return response.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
