fbc58fb7dfe6b6d5c31dec77a54b95fcea780fdb
LeeYongwoo-kor <newlyo1205@naver.com>
2022-04-18 23:44:21 +0900

    Add Post Handler & Screen

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,57 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = request;
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wonderings: true,
+        },
+      },
+    },
+  });
+  response.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,8 +1,34 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { Answer, Post, User } from "@prisma/client";
+import Link from "next/link";
+
+interface AnswerWithUser extends Answer {
+  user: User;
+}
+
+interface PostWithUser extends Post {
+  user: User;
+  _count: {
+    answers: number;
+    wonderings: number;
+  };
+  answers: AnswerWithUser[];
+}
+
+interface CommunityPostResponse {
+  ok: boolean;
+  post: PostWithUser;
+}
 
 const CommunityPostDetail: NextPage = () => {
+  const router = useRouter();
+  const { data, error } = useSWR<CommunityPostResponse>(
+    router.query.id ? `/api/posts/${router.query.id}` : null
+  );
   return (
     <Layout canGoBack>
       <div className="px-4">
@@ -12,10 +38,14 @@ const CommunityPostDetail: NextPage = () => {
         <div className="mb-3 flex items-center space-x-3 border-b pb-3">
           <div className="aspect-square w-10 rounded-full bg-slate-300" />
           <div className="cursor-pointer">
-            <p className="text-sm font-medium text-gray-700">Mr Lee</p>
-            <p className="text-xs font-medium text-gray-600">
-              View profile &rarr;
+            <p className="text-sm font-medium text-gray-700">
+              {data?.post.user.name}
             </p>
+            <Link href={`/users/profiles/${data?.post?.user?.id}`}>
+              <a className="text-xs font-medium text-gray-600">
+                View profile &rarr;
+              </a>
+            </Link>
           </div>
         </div>
         <div>
@@ -42,7 +72,7 @@ const CommunityPostDetail: NextPage = () => {
                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
               ></path>
             </svg>
-            <span>Good 1</span>
+            <span>Good {data?.post?._count.wonderings}</span>
           </span>
           <span className="flex items-center space-x-2 text-sm">
             <svg
@@ -59,22 +89,24 @@ const CommunityPostDetail: NextPage = () => {
                 d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
               ></path>
             </svg>
-            <span>Comment 1</span>
+            <span>Comment {data?.post?._count.answers}</span>
           </span>
         </div>
         <div className="my-5 space-y-5 px-4">
-          <div className="flex items-start space-x-3">
-            <div className="h-8 w-8 rounded-full bg-slate-200" />
-            <div>
-              <span className="block text-sm font-medium text-gray-700">
-                Mr Kim
-              </span>
-              <span className="block text-xs text-gray-500 ">2 Hours ago</span>
-              <p className="mt-2 text-gray-700">
-                The best tteokbokki restaurant is the one next to my house.
-              </p>
+          {data?.post?.answers.map((answer) => (
+            <div key={answer.id} className="flex items-start space-x-3">
+              <div className="h-8 w-8 rounded-full bg-slate-200" />
+              <div>
+                <span className="block text-sm font-medium text-gray-700">
+                  {answer.user.name}
+                </span>
+                <span className="block text-xs text-gray-500 ">
+                  {answer.createdAt}
+                </span>
+                <p className="mt-2 text-gray-700">{answer.answer}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
         <div className="px-4">
           <TextArea
