40a5242b38805f26b9ffae5f5f3ba8ced2bb2fed
LeeYongwoo-kor <newlyo1205@naver.com>
2022-05-07 23:59:00 +0900

    Add Geo Search part

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -7,11 +7,11 @@ async function handler(
   request: NextApiRequest,
   response: NextApiResponse<ResponseType>
 ) {
-  const {
-    body: { question, latitude, longitude },
-    session: { user },
-  } = request;
   if (request.method === "POST") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = request;
     const post = await client.post.create({
       data: {
         question,
@@ -30,6 +30,11 @@ async function handler(
     });
   }
   if (request.method === "GET") {
+    const {
+      query: { latitude, longitude },
+    } = request;
+    const parsedLatitude = parseFloat(latitude.toString());
+    const parsedLongitude = parseFloat(longitude.toString());
     const posts = await client.post.findMany({
       include: {
         user: {
@@ -44,6 +49,16 @@ async function handler(
             wonderings: true,
             answers: true,
           },
+        },
+      },
+      where: {
+        latitude: {
+          gte: parsedLatitude - 0.02,
+          lte: parsedLatitude + 0.02,
+        },
+        longitude: {
+          gte: parsedLongitude - 0.02,
+          lte: parsedLongitude + 0.02,
         },
       },
     });
