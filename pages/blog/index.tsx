fa34472810e867a3c3337b83895836c25194b8eb
LeeYongwoo-kor <newlyo1205@naver.com>
2022-05-28 18:24:47 +0900

    Add getStaticPaths(blog)

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,29 +2,34 @@ import Layout from "@components/layout";
 import matter from "gray-matter";
 import { readdirSync, readFileSync } from "fs";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
   title: string;
   date: string;
   category: string;
+  slug: string;
 }
 
 const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
   return (
     <Layout title="Blog" seoTitle="Blog">
-      <h1 className="text-lg font-semibold">Latest Posts:</h1>
+      <h1 className="mt-5 mb-10 text-center text-lg font-semibold">
+        Latest Posts:
+      </h1>
       <ul>
         {posts.map((post, index) => (
-          <div
-            key={index}
-            className="mb-10 mt-5 text-center text-xl font-semibold"
-          >
-            <span className="text-lg text-red-500">{post.title}</span>
-            <div>
-              <span>
-                {post.date} / {post.category}
-              </span>
-            </div>
+          <div key={index} className="mb-5">
+            <Link href={`/blog/${post.slug}`}>
+              <a>
+                <span className="text-lg text-red-500">{post.title}</span>
+                <div>
+                  <span>
+                    {post.date} / {post.category}
+                  </span>
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
       </ul>
@@ -35,7 +40,8 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
 export async function getStaticProps() {
   const blogPosts = readdirSync("./posts").forEach((file) => {
     const content = readFileSync(`./posts/${file}`, "utf-8");
-    return matter(content).data;
+    const [slug, _] = file.split(".");
+    return { ...matter(content).data, slug };
   });
   return {
     props: {
---
+++ posts/03-third-post.md
@@ -0,0 +1,9 @@
+---
+title: This is Third Post
+data: 2022.05.28
+category: Typescript
+---
+
+# Typescript is so amazing.
+
+really?
